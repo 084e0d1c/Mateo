26bf68fe8e7584dc61a080c67f5d86c847a9c446
Brandon <brandu.tan@gmail.com>
2021-08-29 15:54:27 +0800

    feat: Added params for frontend display

--- backend/loans/user-data/user_profile.py
+++ backend/loans/user-data/user_profile.py
@@ -2,6 +2,7 @@ from os import environ
 
 import boto3
 import simplejson as json
+from decimal import Decimal
 from dynamo_utils import check_pool_eligibility
 from utils import decode_username, exception_handler
 
@@ -29,6 +30,10 @@ def main(event, context):
     
     user_profile = loan_table.get_item(Key={'username': username})['Item']
     
+    total_deposits =  sum([user_profile['deposits_to_pools'][pool_id]['initial_deposit'] for pool_id in user_profile['deposits_to_pools']])
+    total_available =  sum([user_profile['deposits_to_pools'][pool_id]['available'] for pool_id in user_profile['deposits_to_pools']])
+    user_profile['outstanding_inital_deposits'] = total_deposits
+    user_profile['outstanding_available_withdraw'] = total_available
     return {
         "statusCode": "200",
         "body": json.dumps({
